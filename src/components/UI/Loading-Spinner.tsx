8277ecb261f8db34dff4896cd864196918225fe0
Dayakar <dayakarheeraakri@gmail.com>
2023-02-11 22:04:43 +0530

    change gradient of spinner to fit in with the color scheme of the site

--- src/components/UI/Loading-Spinner.tsx
+++ src/components/UI/Loading-Spinner.tsx
@@ -8,7 +8,15 @@ const StyledLoadingSpinner = styled.div`
 	margin-inline: auto;
 	margin-block-start: 7rem;
 	border-radius: 50%;
-	background: conic-gradient(#3dffc9, #f7c723, #eb0ea5, #3dffc9);
+	background: conic-gradient(
+		#d13440ff,
+		#e63946ff,
+		#ec9a9aff,
+		#f1faeeff,
+		#a8dadcff,
+		#457b9dff,
+		#1d3557ff
+	);
 	animation: spin 1.4s linear infinite;
 	box-shadow: 0px 0px 150px -75px black;
 	border: 4px solid white;
